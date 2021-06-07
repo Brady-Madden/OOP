dc04a80fbbbae5605556135185606fc955a4dc97
Brady Madden <81207895+Brady-Madden@users.noreply.github.com>
2021-06-07 16:41:09 -0400

    Create Task.java

---
+++ Problem 4/Task.java
@@ -0,0 +1,18 @@
+/**
+ * The {@code Task} interface serves as the super type for all of the classes defining tasks performed by a
+ * {@code Simulator}. All classes implementing this {@code Task} interface must implement the {@code run()} method which
+ * performs the intended task.
+ * 
+ * @author Jeong-Hyon Hwang (jhh@cs.albany.edu)
+ */
+public interface Task {
+
+	/**
+	 * Performs the intended task. This method is automatically invoked by a {@code Simulator}.
+	 * 
+	 * @param simulationTime
+	 *            the simulation time at which this method is invoked.
+	 */
+	public abstract void run(double simulationTime);
+
+}
